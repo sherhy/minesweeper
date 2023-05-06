913c016614f8e56b200adf371d859e3247ca09ad
hyunjin jeong <hyunjin.jeong@hotmail.com>
2023-05-07 06:09:59 +0900

    feat: print board & configure level

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from random import randrange, sample
-
-
 from enum import Enum, auto
+from functools import cached_property
+from dataclasses import dataclass
+
+
+@dataclass
+class Presets:
+    bomb_count: int
+    rows: int
+    cols: int
 
 
 class Level(Enum):
     EASY = auto()
     DIFFICULT = auto()
+
+
+LEVEL_CONFIGS = {Level.EASY: Presets(10, 10, 10)}
 
 
 class Board:
@@ -15,29 +25,30 @@ class Board:
         self._board = [None for i in range(self.grid_size)]
         self.load_new_bombs()
         self.load_cell_numbers()
+        self.board_visibility_mask = [False for i in range(self.grid_size)]
 
     @property
     def board(self):
         return self._board
 
-    @property
+    @cached_property
     def grid_size(self):
         return self._rows * self._cols
 
     def set_difficulty(self, level: Level):
-        if level == Level.EASY:
-            self._bomb_count = 10
-            self._rows = 10
-            self._cols = 10
+        presets = LEVEL_CONFIGS[level]
+        self._bomb_count = presets.bomb_count
+        self._rows = presets.rows
+        self._cols = presets.cols
 
     def load_new_bombs(self):
         self._loaded_cells = sample(range(self.grid_size), self._bomb_count)
         for i in self._loaded_cells:
-            self.board[i] = "B"
+            self.board[i] = "💣"
 
     def load_cell_numbers(self):
-        for row in range(self._cols):
-            for col in range(self._rows):
+        for row in range(self._rows):
+            for col in range(self._cols):
                 index = row * self._cols + col
                 if index in self._loaded_cells:
                     continue
@@ -65,12 +76,28 @@ class Board:
         nearby_bomb_count = len(set(self._loaded_cells).intersection(neighbor_indices))
         self.board[i] = nearby_bomb_count
 
-    def print_board(self):
+    def print_board(self, debug=True):
+        display_board = [
+            x if self.board_visibility_mask[i] else "X"
+            for i, x in enumerate(self.board)
+        ]
         for row in range(self._rows):
-            print(self.board[(i := row * self._cols) : i + self._cols])
+            board = display_board if not debug else self.board
+            print(
+                "  ".join(
+                    map(
+                        lambda x: f"{x: >2}" if isinstance(x, int) else x,
+                        board[(i := row * self._cols) : i + self._cols],
+                    )
+                )
+            )
+            print()
 
+    def click_cell(self):
+        pass
 
-from pprint import pprint
+    def expand_zero_mine_field(self):
+        pass
 
 
 def main():
